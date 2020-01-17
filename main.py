0e1c7cda89c8e9e60f0e489f2c3c50364cbb6117
Andrey Ovchinnikov <dream.of.yaz@gmail.com>
2020-01-17 10:38:04 +0600

    Create a bot and add some little functional

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-class Main(char str):
-    if (str < str1):
-        
-    
+import telebot
+import sett
+
+bot = telebot.TeleBot(sett.KEY)
+
+
+@bot.message_handler(content_types=["text"])
+def message_get(message):
+    print(message)
+    if message.text == "Привет!":
+        bot.send_message(message.from_user.id, "И тебе привет!")
+    else:
+        bot.send_message(message.from_user.id, "Не понимаю!")
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=5)
