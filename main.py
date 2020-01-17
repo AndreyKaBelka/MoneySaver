5e67a9711242c3da8715a59662a85a04ab123905
Andrey Ovchinnikov <dream.of.yaz@gmail.com>
2020-01-17 11:06:02 +0600

    Commands for bot

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import telebot
 import sett
+from dict import *
 
 bot = telebot.TeleBot(sett.KEY)
 
 
 @bot.message_handler(content_types=["text"])
 def message_get(message):
-    print(message)
-    if message.text == "Привет!":
-        bot.send_message(message.from_user.id, "И тебе привет!")
-    else:
-        bot.send_message(message.from_user.id, "Не понимаю!")
+    pass
+
+
+@bot.message_handler(commands=['start'])
+def welcome_message(message):
+    bot.send_message(message.from_user.id, WELCOME_TEXT)
+
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.from_user.id, HELP_TEXT)
 
 
 if __name__ == "__main__":
