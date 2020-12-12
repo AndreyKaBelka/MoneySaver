9c3d4655cc20d6993b016220dd1663b292fc3d4b
“Andreyka <dream.of.yaz@gmail.com>
2020-12-13 01:04:18 +0600

    add autoincrement to cat_id

--- db/scripts/createTables/categoriesTableCreate.sql
+++ db/scripts/createTables/categoriesTableCreate.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS `moneysaver`.`categories` (
-  `cat_id` INT UNSIGNED NOT NULL,
+  `cat_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `cat_exp` DOUBLE UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`cat_id`),
   UNIQUE INDEX `cat_id_UNIQUE` (`cat_id` ASC) VISIBLE)
 COMMENT = 'Categories table';
-
