3b7d3016c90a9c51b19ef5cb1d655ee70e6cbf22
“Andreyka <dream.of.yaz@gmail.com>
2020-12-12 20:25:17 +0600

    Implement console class

--- console.py
+++ console.py
@@ -1,7 +1,55 @@
-class Console:
-    def __init__(self, args: list[str]):
-        self.args = args
+from typing import List
 
+FLAGS = {
+    "-f": 1
+}
+
+CONSOLE_OPTIONS = {}
+
+
+class Console:
+    def __init__(self, args: List[str]):
+        self.args = args[1:]
+
+    def getData(self):
+        builder = Abstract().start()
+        for arg in self.args:
+            builder = builder.add_attr(arg)
 
     def canRun(self) -> bool:
-        return True
+        return False
+
+
+class Abstract:
+    def start(self):
+        return Flag()
+
+    def add_attr(self, attr):
+        pass
+
+
+class Flag(Abstract):
+    def add_attr(self, attr):
+        CONSOLE_OPTIONS.update({attr: []})
+        if attr not in FLAGS:
+            raise SystemExit(f"Wrong flag: {attr}")
+        if FLAGS.get(attr) > 0:
+            return Args(attr)
+        else:
+            return Flag()
+
+
+class Args(Abstract):
+    def __init__(self, flag):
+        self.flag = flag
+
+    def add_attr(self, attr):
+        if attr in FLAGS:
+            raise SystemExit(f"Wrong options for {self.flag} flag!")
+        args_array = CONSOLE_OPTIONS.get(self.flag)
+        args_array.append(attr)
+        CONSOLE_OPTIONS.update({self.flag: args_array})
+        if FLAGS.get(self.flag) > len(args_array):
+            return Args(self.flag)
+        else:
+            return Flag()
