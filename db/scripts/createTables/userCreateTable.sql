c99d7b0566ee8aa17c05346af011ad700e96047e
“Andreyka <dream.of.yaz@gmail.com>
2020-12-13 03:29:06 +0600

    add scripts to drop and create tables

--- db/scripts/createTables/userCreateTable.sql
+++ db/scripts/createTables/userCreateTable.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS `moneysaver`.`users` (
-  `user_id` INT UNSIGNED NOT NULL,
-  `user_bank` DOUBLE NOT NULL DEFAULT 0,
-  PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE)
-COMMENT = 'User database';
+CREATE TABLE IF NOT EXISTS `moneysaver`.`users`
+(
+    `user_id`   INT UNSIGNED NOT NULL,
+    `user_bank` DOUBLE       NOT NULL DEFAULT 0,
+    PRIMARY KEY (`user_id`),
+    UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE
+)
+    COMMENT = 'User database';
