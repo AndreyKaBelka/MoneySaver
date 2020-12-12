05645d708833cb58a73b154a176332c83fe199ba
“Andreyka <dream.of.yaz@gmail.com>
2020-12-13 00:14:06 +0600

    add sql scripts to create db tables

---
+++ db/scripts/categoriesTableCreate.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `moneysaver`.`categories` (
+  `cat_id` INT UNSIGNED NOT NULL,
+  `cat_exp` DOUBLE UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`cat_id`),
+  UNIQUE INDEX `cat_id_UNIQUE` (`cat_id` ASC) VISIBLE)
+COMMENT = 'Categories table';
+
---
+++ db/scripts/tagsTableCreate.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS `moneysaver`.`tags` (
+  `tag_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `cat_id` INT UNSIGNED NOT NULL,
+  `tag_desc` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`tag_id`),
+  UNIQUE INDEX `tag_id_UNIQUE` (`tag_id` ASC) VISIBLE)
+COMMENT = 'Tags for category';
+ALTER TABLE `moneysaver`.`tags`
+ADD INDEX `cat_id_idx` (`cat_id` ASC) VISIBLE;
+ALTER TABLE `moneysaver`.`tags`
+ADD CONSTRAINT `cat_id`
+  FOREIGN KEY (`cat_id`)
+  REFERENCES `moneysaver`.`categories` (`cat_id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
